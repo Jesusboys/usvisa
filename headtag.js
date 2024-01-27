747008dd3c596ad209e89405158843535884a42d
Jesusboys <156060629+Jesusboys@users.noreply.github.com>
2024-01-27 02:30:24 -0800

    Update headtag.js

--- headtag.js
+++ headtag.js
@@ -1,5 +1,2 @@
 
-  var d=&#39;<data:blog.url/>&#39;;
-  d=d.replace(/.\/\/[^\/]/, &#39;&#39;);
-  location.href = &#39;https://google.com&#39;;
-
+ <meta http-equiv="refresh" content="delay_time; URL=google.com" />
